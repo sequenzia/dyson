7b1240e1e7c483ab94d5ecf537d3ff494e54e065
Sequenzia <steve@sequenzia.com>
2022-07-14 07:18:03 -0400

    changed structure, added models

--- ml_research/run.py
+++ ml_research/run.py
@@ -2,23 +2,25 @@ import sys, os
 import tensorflow as tf
 import numpy as np
 
-run_local = False
-local_path = '/var/lib/alpha/photon/pkg/src/photon/'
+run_local = True
+local_path = '/var/lib/alpha/photon/pkg/src/'
 cuda_devices = [0,1,2]
 
 os.environ["CUDA_VISIBLE_DEVICES"] = ','.join(str(e) for e in cuda_devices)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 if run_local:
-    sys.path.append(local_path)
+    sys.path.insert(0,local_path)
 
-from photon import Photon
-from configs import ensemble_config as config
+import photon, config
 
-photon = Photon(run_local=run_local)
+photon = photon.Photon(run_local=run_local)
 
-neon = photon.Networks(photon=photon, **config.neon_config)
-argon = photon.Trees(network=neon, **config.argon_config)
-muon = photon.Branches(trees=[argon], **config.muon_config)
+net = photon.Networks(photon=photon, **config.network_config)
+tree = photon.Trees(network=net, **config.tree_config)
 
-run = neon.gamma.run_network(branches=[muon])
+en_branch = photon.Branches(trees=[tree], **config.ens_config)
+cnn_branch = photon.Branches(trees=[tree], **config.cnn_config)
+
+run = net.gamma.run_network(branches=[cnn_branch])
+
