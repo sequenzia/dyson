2b954a562b4f5bb19d9a7c0810197ae29a3412ad
Steve Sequenzia <steve@sequenzia.com>
2023-06-18 02:22:35 +0000

    colab_setup update

--- colab_setup.sh
+++ colab_setup.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-TEMP_DIR=~/.temp
-SRC_DIR=~/src
+TEMP_DIR=/home/zadmin/.temp
+SRC_DIR=/home/zadmin/src
 
-if [ -d "$TEMP_DIR" ]; then
-rm -r $TEMP_DIR
+if [ -d $TEMP_DIR ]; then
+    chmod 777 -R $TEMP_DIR
+    rm -r $TEMP_DIR
 fi
 
-if [ -d "$SRC_DIR" ]; then
-rm -r $SRC_DIR
+if [ -d $SRC_DIR ]; then
+    chmod 777 -R $SRC_DIR
+    rm -r $SRC_DIR
 fi
 
 mkdir $TEMP_DIR
 mkdir $SRC_DIR
 
-git clone https://github.com/sequenzia/photon $TEMP_DIR
-git clone https://github.com/sequenzia/photon $TEMP_DIR
+git clone https://github.com/sequenzia/photon $TEMP_DIR/photon
+git clone https://github.com/sequenzia/dyson $TEMP_DIR/dyson
+
+cp -r $TEMP_DIR/photon/photon $SRC_DIR/photon
+cp -r $TEMP_DIR/dyson/dyson $SRC_DIR/dyson
