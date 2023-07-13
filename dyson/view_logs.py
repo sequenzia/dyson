f1b48fd5ca8639e81046946fff8f03cb01f5caf6
Steve Sequenzia <steve@sequenzia.com>
2023-07-13 04:51:41 +0000

    trans dev work

--- dyson/view_logs.py
+++ dyson/view_logs.py
@@ -20,30 +20,48 @@ gauge = chain.models[model_idx]
 
 batch_logs = chain.logs.batch_data['main']
 
+batch_logs[0][0].keys()
+
+inputs = batch_logs[0][0]['inputs']
+targets = batch_logs[0][0]['targets']
+tracking = batch_logs[0][0]['tracking']
+outputs = batch_logs[0][0]['outputs']
+
+
+
+inputs[0][:25]
+
+outputs[0][:25]
+
+
 # --- Call Logs --- #
 
 # --- Layer Logs --- #
 """
     gauge.logs.layers[log_type][epoch_idx][batch_idx][layer_idx]
-    
+
     keys = ['step' : [chain_idx, epoch_idx, batch_idx],
             'layer',
             'layer_idx',
-            'layer_name', 
+            'layer_name',
             'in_shape',
             'out_shape',
             'input',
             'output',
             'loss']
-            
+
     for x in layer_logs:
         print(x['layer_name'], x['in_shape'], x['out_shape'])
-                   
+
 """
 
-layer_logs = gauge.logs.layers['main'][0][0]
+layer_logs = gauge.logs.layers['main'][0][0][2]
 
-layer_logs[0]
+print(layer_logs['layer_name'])
+print(layer_logs['in_shape'])
+print(layer_logs['out_shape'])
+
+
 
 # --- Run Data Logs -- #
 """ """
@@ -51,13 +69,13 @@ layer_logs[0]
 # ---  Theta Logs --- #
 """
     gauge.logs.theta[epoch_idx][batch_idx]
-    
+
     keys = ['model_pre', 'model_post', 'opt', 'grads']
-    
+
     tf.subtract(pre,tf.math.multiply(lr,grads))
-    
+
     tf.math.reduce_sum([pre, - tf.math.mulgrads])
-    
+
     tf.math.reduce_sum(post)
 
 """
